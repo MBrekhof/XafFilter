219c5f9553ab9bb686116127ac7ea254a9dfe9f2
MBrekhof <martin.brekhof@gmail.com>
2026-05-16 21:36:30 +0200

    feat: add Customer demo BO

---
+++ XafFilter/XafFilter.Module/BusinessObjects/Demo/Customer.cs
@@ -0,0 +1,16 @@
+using DevExpress.Persistent.BaseImpl.EF;
+using System.ComponentModel;
+
+namespace XafFilter.Module.BusinessObjects.Demo;
+
+[DefaultProperty(nameof(Name))]
+public class Customer : BaseObject
+{
+    public virtual string Name { get; set; } = string.Empty;
+    public virtual string? Email { get; set; }
+    public virtual string? Company { get; set; }
+    public virtual bool IsVip { get; set; }
+    public virtual DateTime CreatedAt { get; set; }
+
+    public virtual IList<Ticket> Tickets { get; set; } = new List<Ticket>();
+}
