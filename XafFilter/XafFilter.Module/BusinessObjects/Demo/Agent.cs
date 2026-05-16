39b399e1044da03b2778986022750f856a1b8219
MBrekhof <martin.brekhof@gmail.com>
2026-05-16 21:37:33 +0200

    feat: add Agent demo BO

---
+++ XafFilter/XafFilter.Module/BusinessObjects/Demo/Agent.cs
@@ -0,0 +1,15 @@
+using DevExpress.Persistent.BaseImpl.EF;
+using System.ComponentModel;
+
+namespace XafFilter.Module.BusinessObjects.Demo;
+
+[DefaultProperty(nameof(DisplayName))]
+public class Agent : BaseObject
+{
+    public virtual string DisplayName { get; set; } = string.Empty;
+    public virtual string? Email { get; set; }
+    public virtual bool IsActive { get; set; }
+    public virtual int HoursPerWeek { get; set; }
+
+    public virtual IList<Ticket> AssignedTickets { get; set; } = new List<Ticket>();
+}
